b792dac67c18d1c74053f3c77370b1b6c1cb646e
Dack-eng <gggantulga123@gmail.com>
2026-03-17 00:34:48 +0800

    Optimize deployment config: Add standalone output and test page

---
+++ app/test/page.tsx
@@ -0,0 +1,7 @@
+export default function TestPage() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-black text-white">
+      <h1 className="text-4xl font-bold italic text-primary">MANGA MN TEST</h1>
+    </div>
+  );
+}
